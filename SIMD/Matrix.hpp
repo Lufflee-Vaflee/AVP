b813f32fbd007dcbed762afb363bae0812b4f333
a.ostrovsky <a.ostrovsky@lacit.net>
2024-05-13 19:13:04 +0300

    dev

--- SIMD/Matrix.hpp
+++ SIMD/Matrix.hpp
@@ -56,6 +56,26 @@ struct Span {
         return get_i(i) + j; 
     }
 
+    constexpr auto splitByN() {
+        std::size_t newN1 = N / 2;
+        std::size_t newN2 = N - newN1;
+
+        return std::pair(
+            Span<N_origin, M_origin>(N_offset, M_offset, newN1, M),
+            Span<N_origin, M_origin>(N_offset + newN1, M_offset, newN2, M)
+        );
+    }
+
+    constexpr auto splitByM() {
+        std::size_t newM1 = M / 2;
+        std::size_t newM2 = M - newM1;
+
+        return std::pair(
+            Span<N_origin, M_origin>(N_offset, M_offset, N, newM1),
+            Span<N_origin, M_origin>(N_offset, M_offset + newM1, N, newM2)
+        );
+    }
+
     std::size_t N_offset;
     std::size_t M_offset;
     std::size_t N;
@@ -75,7 +95,23 @@ struct multTraits {
     Span<M, P> B_Span;
 
     constexpr auto splitByN() {
-         
+        std::size_t newN1 = C_Span.N / 2;
+        std::size_t newN2 = C_Span.N - newN1;
+
+        std::size_t newN_offset = C_Span.N_offset + newN1;
+
+        auto newC_Span1 = C_Span;
+        auto newA_Span1 = A_Span;
+        newC_Span1.N = newN1;
+        newA_Span1.M = newN1;
+
+        auto newC_Span2 = Span<N, P> {C_Span.N_offset + newN1, C_Span.M_offset, newN2, C_Span.M};
+        auto newA_Span2 = Span<N, M> {A_Span.N_offset + newN1, A_Span.M_offset, newN2, A_Span.M};
+
+        return std::pair(
+            multTraits<N, M, P> {newC_Span1, newA_Span1, B_Span},
+            multTraits<N, M, P> {newC_Span2, newA_Span2, B_Span}
+        );
     }
 };
 
@@ -106,9 +142,6 @@ void mult_body(element* C, element* A, element* B) {
     if constexpr (max == N) {
 
 
-
-
-
     } else if constexpr (max == M) {
 
 
@@ -116,6 +149,6 @@ void mult_body(element* C, element* A, element* B) {
     } else {
 
     }
-};
+}
 
 }
